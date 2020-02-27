cda5f8210dc05912bc4e1166973e97e66b1b1a8c
Arpit <arpit@revolution.ca>
2020-02-27 13:41:58 -0400

    added 'tasks' collection to the db and a few new documents to the db as well

--- mongodb.js
+++ mongodb.js
@@ -1,7 +1,6 @@
 // CRUD create read update delete
 
-const mongodb = require('mongodb')
-const MongoClient = mongodb.MongoClient
+const { MongoClient, ObjectID } = require('mongodb')
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
@@ -12,8 +11,27 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: 
     }
 
     const db = client.db(databaseName)
-    db.collection('users').insertOne({
-        name: 'Arpit',
-        age: 20
+
+    // db.collection('users').findOne({ name: { $regex: RegExp('Kev', 'i') } }, (error, user) => {
+    //     if (error) {
+    //         return console.log('Something went wrong!' + error)
+    //     }
+
+    //     console.log(user)
+    // })
+
+    // db.collection('users').find({ age: 25 }).toArray((error, users) => {
+    //     console.log(users)
+    // })
+    // db.collection('users').find({ age: 25 }).count((error, countUsers) => {
+    //     console.log(countUsers)
+    // })
+
+    db.collection('tasks').findOne({ _id: ObjectID("5e57f4a127620c1bfcec499d") }, (error, task) => {
+        console.log(task)
+    })
+
+    db.collection('tasks').find({ completed: false }).toArray((error, tasks) => {
+        console.log(tasks)
     })
 })
