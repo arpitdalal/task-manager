4c5e780ca4b4d9a4adae763815b9f40e09e53f6e
Arpit <arpit@revolution.ca>
2020-02-27 12:40:25 -0400

    initial commit with mongodb pckg and code to connect to mongodb

---
+++ mongodb.js
@@ -0,0 +1,19 @@
+// CRUD create read update delete
+
+const mongodb = require('mongodb')
+const MongoClient = mongodb.MongoClient
+
+const connectionURL = 'mongodb://127.0.0.1:27017'
+const databaseName = 'task-manager'
+
+MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
+    if (error) {
+        return console.log('Something went wrong!\n' + error)
+    }
+
+    const db = client.db(databaseName)
+    db.collection('users').insertOne({
+        name: 'Arpit',
+        age: 20
+    })
+})
