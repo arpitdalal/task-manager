2b595687350c9ab7fef0fa4594597cc27101b5f6
Arpit <arpitdalal21@gmail.com>
2020-05-26 16:56:21 -0300

    added sharp module

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const multer = require('multer')
-require('../db/mongoose')
+const sharp = require('sharp')
 const User = require('../models/user')
 const auth = require('../middleware/auth')
 const router = new express.Router()
