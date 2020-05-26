dfabcaad891ec3e1becc44e194955cd044b9454b
Arpit <arpitdalal21@gmail.com>
2020-05-26 17:02:52 -0300

    added func to crop image to 250x250 and convert it to a png

--- src/routers/user.js
+++ src/routers/user.js
@@ -68,7 +68,8 @@ const upload = multer({
     }
 })
 router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) => {
-    req.user.avatar = req.file.buffer
+    const buffer = await sharp(req.file.buffer).resize({ width: 250, height: 250 }).png().toBuffer()
+    req.user.avatar = buffer
     await req.user.save()
     res.send()
 }, (error, req, res, next) => {
@@ -88,7 +89,7 @@ router.get('/users/:id/avatar', async (req, res) => {
         if(!user || !user.avatar){
             throw new Error()
         }
-        res.set('Content-Type', 'image/jpg')
+        res.set('Content-Type', 'image/png')
         res.send(user.avatar)
     }catch (e){
         res.sendStatus(404)
